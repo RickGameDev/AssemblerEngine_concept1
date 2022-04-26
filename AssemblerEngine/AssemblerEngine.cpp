ff13a61c8c3232b68ab38df9308e3cb67a958b7f
Rick Nijhuis <rick.nijhuis@sycade.com>
2022-04-26 11:42:54 +0200

    Add project files.

---
+++ AssemblerEngine/AssemblerEngine.cpp
@@ -0,0 +1,12 @@
+﻿// AssemblerEngine.cpp : Defines the entry point for the application.
+//
+
+#include "AssemblerEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
