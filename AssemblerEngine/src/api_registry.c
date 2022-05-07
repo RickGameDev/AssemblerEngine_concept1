bfa43820cf9b859708139e9c19d33424e8e78251
Rick Nijhuis <rick.nijhuis@sycade.com>
2022-05-07 14:35:35 +0200

    fix not setting api

--- AssemblerEngine/src/api_registry.c
+++ AssemblerEngine/src/api_registry.c
@@ -6,88 +6,11 @@
 
 #include "hashmap.h"
 
-//struct hash_node
-//{
-//	struct hash_node* next;
-//	uint32_t key_hash;
-//	void* value;
-//	char key[1];
-//};
-//
-//struct hash_table
-//{
-//	struct hash_node** buckets;
-//	uint32_t size;
-//	uint32_t count;
-//	uint32_t mask;
-//	uint32_t max_chain;
-//};
-
 struct ae_api_registry_o
 {
 	struct ae_hashmap apis;
 	struct ae_hashmap interfaces;
 };
-
-// fnva1 hash
-//static uint32_t hash_string(const char* str, uint32_t length)
-//{
-//	unsigned char* p = (unsigned char*)str;
-//	unsigned long int h = 2166136261UL;
-//	unsigned long int i;
-//
-//	for (i = 0; i < length; i++)
-//		h = (h ^ 16777619) * p[i];
-//
-//	return h;
-//}
-//
-//static struct hash_node* hash_table_find(struct hash_table* table, const char* key, uint32_t hash, uint32_t key_length)
-//{
-//	struct hash_node* node = NULL;
-//
-//	for (node = table->buckets[hash & table->mask]; node != NULL; node = node->next)
-//	{
-//		if (node->key_hash == hash &&
-//			strncmp(node->key, key, key_length) == 0 &&
-//			node->key[key_length] == '\0')
-//			break;
-//	}
-//
-//	return node;
-//}
-//
-//static void hash_table_resize(struct hash_table* table)
-//{
-//	struct hash_node** buckets = table->buckets;
-//
-//	uint32_t new_size = 2 * table->size;
-//	uint32_t new_mask = new_size - 1;
-//
-//	if (!(table->buckets = malloc(sizeof(*table->buckets) * new_size)))
-//		return;
-//
-//	memset(table->buckets, 0, sizeof(*table->buckets) * new_size);
-//
-//	uint32_t i = 0;
-//
-//	for (; i < table->size; i++)
-//	{
-//		struct hash_node* node = buckets[i];
-//		while (node != NULL)
-//		{
-//			struct hash_node* next = node->next;
-//			node->next = table->buckets[node->key_hash & new_mask];
-//			table->buckets[node->key_hash & new_mask] = node;
-//			node = next;
-//		}
-//	}
-//
-//	free(buckets);
-//	
-//	table->size = new_size;
-//	table->mask = new_mask;
-//}
 
 struct ae_api_registry_api* ae_api_registry_new()
 {
@@ -124,6 +47,10 @@ struct ae_api_registry_api* ae_api_registry_new()
 		free(self);
 		return NULL;
 	}
+
+	self->get_api = ae_api_registry_get_api;
+	self->set_api = ae_api_registry_set_api;
+	self->remove_api = ae_api_registry_remove_api;
 
 	return self;
 }
